491738a46feab2fce4d7db943274f37e01f99af8
Shubham Mendapara <58872826+Shubham-2441@users.noreply.github.com>
2023-10-19 02:29:37 -0400

    Add Home

---
+++ src/Kanbas/Courses/index.js
@@ -0,0 +1,40 @@
+import db from "../../Kanbas/Database";
+import { Navigate, Route, Routes, useParams } from "react-router-dom";
+import CourseNavigation from "./CourseNavigation";
+import "./index.css";
+import Breadcrumb from "./Components/Breadcrumb";
+import Modules from "../Modules";
+import Home from "../Home";
+import Assignments from "../Assignments";
+import AssignmentEditor from "../Assignments/AssignmentEditor";
+
+function Courses() {
+  const { courseId } = useParams();
+  const course = db.courses.find((course) => course._id === courseId);
+  return (
+    <div className="row">
+      <Breadcrumb courseName={course.name} />
+      <hr style={{ marginLeft: "15px", marginTop: "20px", width: "95%" }} />
+      <CourseNavigation />
+      <div className="col">
+        <div
+          // className="overflow-y-scroll position-fixed bottom-0 end-0"
+          className="bottom-0 end-0"
+        >
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route
+              path="Assignments/:assignmentId/*"
+              element={<AssignmentEditor />}
+            />
+            <Route path="Grades" element={<h1>Grades</h1>} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Courses;
--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,4 +1,4 @@
-import Nav from "../Nav";
+import Courses from "./Courses";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./KanbasNavigation";
 import { Routes, Route, Navigate } from "react-router-dom";
@@ -12,7 +12,7 @@ function Kanbas() {
           <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="Account" element={<h1>Account</h1>} />
           <Route path="Dashboard" element={<Dashboard />} />
-          <Route path="Courses/:courseId/*" element={<h1>Courses</h1>} />
+          <Route path="Courses/:courseId/*" element={<Courses />} />
         </Routes>
       </div>
     </div>
